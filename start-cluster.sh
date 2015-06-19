713eaf9ebd51ade214b82f0b097db882bb286d8d
YIWEI-CHEN <feberium@gmail.com>
2015-06-19 14:42:04 +0800

    1. add build.sh in hadoop-dn & hadoop-dn-nn to build images 2. update start-cluster.sh by using my images

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -t --dns 127.0.0.1 -e NODE_TYPE=s -P --name slave1 -h slave1.mycorp.kom alvinhenrick/hadoop-dn
+docker run -d -t --dns 127.0.0.1 -e NODE_TYPE=s -P --name slave1 -h slave1.mycorp.kom yiwei1012/ubuntu-hadoop-dn:1.0
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
-docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom alvinhenrick/hadoop-nn-dn
+docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom yiwei1012/ubuntu-hadoop-nn-dn:1.0
 
